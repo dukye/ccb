516a5ab1a32014dfcc90456e2b0b34b7810e0a17
dooke <me@duke.fr>
2017-11-07 23:54:20 +0100

    index

--- index.js
+++ index.js
@@ -114,12 +114,14 @@ bot.on('message', message => {
 bot.login(process.env.DISCORDBOT);
 
 
+/*
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World\n');
 }).listen(process.env.PORT || 8080, '127.0.0.1');
 console.log('Server currently listening...');
+*/
 
 /*
 var args = {
