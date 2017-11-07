64f695024982ac876fb0885904811371fcc02c98
dooke <me@duke.fr>
2017-11-08 00:32:52 +0100

    index

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bot = new Discord.Client();
 
 var api = new Client();
 
-const getTrade = async (currencypair, exchange = 'kraken') => {
+const getTrade = async function (currencypair, exchange = 'kraken') => {
   let options = null;
   if (exchange === 'gdax') {
     options = {
@@ -30,7 +30,7 @@ const getTrade = async (currencypair, exchange = 'kraken') => {
   return (options ? await rp(options) : false);
 }
 
-const getAssetPairs = async () => {
+const getAssetPairs = async function () => {
   // https://api.gdax.com/products
   const options = {
     method: 'GET',
